c1ddba25bd6e52a7e5e762151d038a415b232464
collinsitua <collinsitua38@gmail.com>
2023-03-23 15:45:19 -0700

    positive

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -11,9 +11,11 @@
 void positive_or_negative(int i)
 
 {
-	if (i < 98)
+	if (i < 0)
+		printf("%d is positive\n", i);
+	else if (i < 0)
 		printf("%d is negative\n", i);
 	else
-		printf("%d is positive\n", i);
+		printf("%d is zero\n", i);
 }
 
